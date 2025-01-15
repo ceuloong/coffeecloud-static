76f1fabdc8dc30c8314e48d47a1c4ecf4289f4bc
ceuloong <shawn.ceuj@gmail.com>
2025-01-15 14:09:23 +0800

    update

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,8 +1,9 @@
 import mysql from 'mysql2/promise';
-import { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } from '$env/static/private';
+import { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } from '$env/static/private';
 
 export const pool = mysql.createPool({
   host: DB_HOST,
+  port: Number(DB_PORT),
   user: DB_USER,
   password: DB_PASSWORD,
   database: DB_NAME,
